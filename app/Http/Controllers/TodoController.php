3135ab90ed46363b993af16ef40b970716c9ab25
Hsueh <40841121@gm.nfu.edu.tw>
2021-12-06 18:33:33 +0800

    read todos資料表的資料，並回傳到pages.index

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $data = DB::table('todos')->get();
+//        dd($data);
+        return view('pages.index',compact('data',$data));
+    }
 
         return view('pages.index');
     }
