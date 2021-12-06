e45f09fe3fd7439525b0e9d62c10a5254aabb06e
Hsueh <40841121@gm.nfu.edu.tw>
2021-12-06 18:17:21 +0800

    建立 create.blade

---
+++ resources/views/pages/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+@section('content')
+    <form>
+        <label>標題</label>
+        <input name="title">
+        <label>內容</label>
+        <input name="content">
+        <label>備註</label>
+        <input name="remark">
+        <button type="submit">submit</button>
+    </form>
+
+@endsection
