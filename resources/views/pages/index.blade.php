0efdb61e00e6149a94fb24958bc3dd82fd5d58b7
Hsueh <40841121@gm.nfu.edu.tw>
2021-12-06 18:17:35 +0800

    加入 todos 的資料

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,4 +1,21 @@
 @extends('layouts.master')
 @section('content')
-    123222
+    <table class="table">
+        <thead>
+        <tr>
+            <td>標題</td>
+            <td>內容</td>
+            <td>備註</td>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($data as $row)
+            <tr>
+                <td>{{$row->title}}</td>
+                <td>{{$row->content}}</td>
+                <td>{{$row->remark}}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 @endsection
